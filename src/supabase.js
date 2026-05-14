8abaeb9d4b0a2ab6d7ce05297927026a8c3da366
mkvsr <v.soria@maneko.fr>
2026-05-14 18:25:16 +0200

    Create supabase.js

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://fkzligxuqhgegbxbeklu.supabase.co",
+  "sb_publishable_ye6pNWjnJmr0qly9qliK7A_cw1TDAmg"
+);
