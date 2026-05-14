b80de9cf172ab8eedf89ad6079fc37d73b241766
mkvsr <v.soria@maneko.fr>
2026-05-14 18:40:46 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   "https://fkzligxuqhgegbxbeklu.supabase.co",
-  "sb_publishable_ye6pNWjnJmr0qly9qliK7A_cw1TDAmg"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZremxpZ3h1cWhnZWdieGJla2x1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NjQ4MDYsImV4cCI6MjA5NDM0MDgwNn0.GFyA6plvtS8m7N0IBagUiTPPNEwwBxYbspPRcOOSPmw"
 );
